b6deba3346e36065cb28bb9ca9f85494ac2f03a2
CHERRADI AZIZ Taza - Taounate - Al Hoceima <aziz.cherradi@taalim.ma>
2025-11-10 16:14:49 +0100

    pics done

--- shop/index.js
+++ shop/index.js
@@ -1,18 +1,18 @@
 // Check if user is logged in
 if (localStorage.getItem('isLoggedIn') !== 'true') {
-    window.location.href = '/login';
+    window.location.href = '../login/index.html';
 }
 
 // All products data
 const allProducts = [
-    { id: 1, name: 'Wireless Headphones', price: 79.99, Image: 'C:/Users/CHERRADIAZIZTaza-Tao/Desktop/IMG/OIP.webp' },
-    { id: 2, name: 'Smart Watch', price: 199.99, emoji: '⌚' },
-    { id: 3, name: 'Laptop', price: 999.99, emoji: '💻' },
-    { id: 4, name: 'Camera', price: 549.99, emoji: '📷' },
-    { id: 5, name: 'Smartphone', price: 699.99, emoji: '📱' },
-    { id: 6, name: 'Tablet', price: 449.99, emoji: '📲' },
-    { id: 7, name: 'Gaming Console', price: 399.99, emoji: '🎮' },
-    { id: 8, name: 'Keyboard', price: 89.99, emoji: '⌨️' }
+    { id: 1, name: 'Wireless Headphones', price: 79.99, image:'assets/OIP.webp'},
+    { id: 2, name: 'Smart Watch', price: 199.99, image:'assets/Smartwatch.jpg' },
+    { id: 3, name: 'Laptop', price: 999.99, image:'assets/laptop.jpg' },
+    { id: 4, name: 'Camera', price: 549.99, image:'assets/cameracanon.webp' },
+    { id: 5, name: 'Apple Iphone 17', price: 900, image:'assets/iphone_17.webp' },
+    { id: 6, name: 'Ipad pro 6', price: 999, image:'assets/ipad.jpg' },
+    { id: 7, name: 'Gaming Console', price: 399.99, image:'assets/gamingconsole.jpg' },
+    { id: 8, name: 'Keyboard', price: 89.99, image:'assets/keyboard.webp' }
 ];
 
 // Update cart count
